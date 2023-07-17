2ecf3a15df1a35c7cad3026a429f6b6ae70ab0b5
Ihor Poliakov <piv2003@gmail.com>
2023-07-17 12:11:36 +0200

    create index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from 'GlobalStyles';
+import { App } from 'components/App/App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
+      <App />
+      <GlobalStyle />
+    </BrowserRouter>
+  </React.StrictMode>
+);
