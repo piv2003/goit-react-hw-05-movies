a89b313d50e393fff25f28da2e025018d2aa16a3
Ihor Poliakov <piv2003@gmail.com>
2023-07-17 18:35:15 +0200

    BASE_URL & TOKEN

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,1 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const TOKEN =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MWNmYjU2NzQ5NGJlZTU3YTk2NTQ4NDJmZGM2Nzg2YSIsInN1YiI6IjY0YjU1YWY5MGJiMDc2MDEyZDU5MTE1NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PuLGboHINRczyDWMglAFi0IkvOIuO777DQSb9p7AEjM';
