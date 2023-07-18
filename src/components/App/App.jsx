5c1490f73a306768407442ca09111a8d1146ca8e
Ihor Poliakov <piv2003@gmail.com>
2023-07-18 23:16:09 +0200

    adding paths

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,3 +4,22 @@ import axios from 'axios';
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const TOKEN =
   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MWNmYjU2NzQ5NGJlZTU3YTk2NTQ4NDJmZGM2Nzg2YSIsInN1YiI6IjY0YjU1YWY5MGJiMDc2MDEyZDU5MTE1NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PuLGboHINRczyDWMglAFi0IkvOIuO777DQSb9p7AEjM';
+
+export const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="trailer" element={<MovieTrailer />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+      <ToastContainer autoClose={2500} theme="dark" />
+    </div>
+  );
+};
