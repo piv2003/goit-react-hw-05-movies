d3f21e11d3065c1e05182e76c62c83605f339ddc
Ihor Poliakov <piv2003@gmail.com>
2023-07-20 21:58:31 +0200

    Update Skeleton.js

--- src/components/Skeleton/Skeleton.js
+++ src/components/Skeleton/Skeleton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import ContentLoader from 'react-content-loader';
+
+const Skeleton = ({ ...props }) => {
+  const rowCount = 4; // Number of rows
+  const columnCount = 5; // Number of columns
+  const rectWidth = 250;
+  const rectHeight = 360;
+  const rectSpacingX = 265; //Horizontal spacing between rectangles
+  const rectSpacingY = 375; //Vertical spacing between rectangles
+  const rectanglesCount = rowCount * columnCount;
+};
