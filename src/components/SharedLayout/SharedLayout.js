95bfdc2ac1585f8d099d723581e5eafb31626005
Ihor Poliakov <piv2003@gmail.com>
2023-07-20 23:30:46 +0200

    adding SharedLayout.styled.jsx

--- src/components/SharedLayout/SharedLayout.js
+++ src/components/SharedLayout/SharedLayout.js
@@ -1,3 +1,24 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header, Link } from './SharedLayout.styled';
+
+const SharedLayout = () => {
+  return (
+    <>
+      <Header>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/movies">Movies</Link>
+        </nav>
+      </Header>
+
+      <main>
+        <Suspense fallback={<h2>Loading...</h2>}>
+          <Outlet />
+        </Suspense>
+      </main>
+    </>
+  );
+};
+
+export default SharedLayout;
---
+++ src/components/SharedLayout/SharedLayout.styled.jsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 12px;
+  padding: 8px 0;
+  margin-bottom: 16px;
+  border-bottom: 1px solid black;
+
+  > nav {
+    display: flex;
+  }
+`;
+
+export const Link = styled(NavLink)`
+  padding: 8px 16px;
+  border-radius: 4px;
+  text-decoration: none;
+  color: black;
+  font-weight: 500;
+  transition: color 250ms cubic-bezier(0.4, 0, 0.2, 1),
+    background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  &.active {
+    color: white;
+    background-color: orangered;
+  }
+`;
