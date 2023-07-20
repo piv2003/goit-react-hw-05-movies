3635d924c7622cd4f6ae6e1ffe6f8c9394796524
Ihor Poliakov <piv2003@gmail.com>
2023-07-20 22:00:20 +0200

    Update Skeleton.js

--- src/components/Skeleton/Skeleton.js
+++ src/components/Skeleton/Skeleton.js
@@ -9,6 +9,20 @@ const Skeleton = ({ ...props }) => {
   const rectSpacingX = 265; //Horizontal spacing between rectangles
   const rectSpacingY = 375; //Vertical spacing between rectangles
   const rectanglesCount = rowCount * columnCount;
+
+  return (
+    <ContentLoader
+      speed={2}
+      width={1400}
+      height={2000}
+      viewBox="0 0 1400 2000"
+      backgroundColor="#d9d9d9"
+      foregroundColor="#fbfbfb"
+      {...props}
+    >
+      {generateRectangles()}
+    </ContentLoader>
+  );
 };
 
 export default Skeleton;
