a8ad02bcd38f34e8b8e2cac4d91ef8abd8aa6484
Ihor Poliakov <piv2003@gmail.com>
2023-07-20 22:06:18 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
 
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const TOKEN =
   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MWNmYjU2NzQ5NGJlZTU3YTk2NTQ4NDJmZGM2Nzg2YSIsInN1YiI6IjY0YjU1YWY5MGJiMDc2MDEyZDU5MTE1NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PuLGboHINRczyDWMglAFi0IkvOIuO777DQSb9p7AEjM';
+
+const Home = lazy(() => import('../../pages/Home'));
+const Movies = lazy(() => import('../../pages/Movies'));
+const MovieDetails = lazy(() => import('../../pages/MovieDetails'));
+const Cast = lazy(() => import('../Cast/Cast'));
+const Reviews = lazy(() => import('../Reviews/Reviews'));
+const MovieTrailer = lazy(() => import('../MovieTrailer/MovieTrailer'));
 
 export const App = () => {
   return (
