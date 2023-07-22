43613eb1075d8984371e47d75a4392f991eb3438
Ihor Poliakov <piv2003@gmail.com>
2023-07-23 00:03:08 +0200

    Update Reviews.js

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -3,3 +3,11 @@ import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getMovieDetails } from '../App/App';
 import { List, DoNotHave } from './Reviews.styled';
+
+const Reviews = () => {
+  const { movieId } = useParams();
+  const [reviews, setReviews] = useState({});
+  const [quantityReviews, setQuantityReviews] = useState(null);
+};
+
+export default Reviews;
