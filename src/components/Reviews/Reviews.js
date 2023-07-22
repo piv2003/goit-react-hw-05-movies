17723e370165b021378be7da9298c54adf49d75e
Ihor Poliakov <piv2003@gmail.com>
2023-07-23 00:02:30 +0200

    adding Reviews

---
+++ src/components/Reviews/Reviews.js
@@ -0,0 +1,5 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getMovieDetails } from '../App/App';
+import { List, DoNotHave } from './Reviews.styled';
