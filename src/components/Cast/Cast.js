0995cf2217d7b598f2d9a8940bf8a5c3f2be8855
Ihor Poliakov <piv2003@gmail.com>
2023-07-22 13:39:08 +0200

    Update Cast.js

--- src/components/Cast/Cast.js
+++ src/components/Cast/Cast.js
@@ -3,3 +3,5 @@ import { useParams } from 'react-router-dom';
 import { getMovieDetails } from '../App/App';
 import { List } from './Cast.styled';
 import ScrollToTopButton from '../ScrollToTopButton/ScrollToTopButton.js';
+
+export default Cast;
