064d095dc6d9c94db450058ec5881c051f02e32e
Ihor Poliakov <piv2003@gmail.com>
2023-07-22 23:54:07 +0200

    Update MovieDetails.styled.jsx

---
+++ src/components/MovieDetails/MovieDetails.styled.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+export const Container = styled.div`
+  margin: 20px auto;
+  padding: 10px;
+  background-color: #f8faea;
+`;
+
+export const MovieInfo = styled.div`
+  display: flex;
+  gap: 15px;
+`;
