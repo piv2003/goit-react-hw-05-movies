669b27a898f65b1d543f2e26061c722aceda8c12
Ihor Poliakov <piv2003@gmail.com>
2023-07-22 23:56:46 +0200

    adding MovieList

---
+++ src/components/MovieList/MovieList.js
@@ -0,0 +1,4 @@
+import { Link, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Item, List } from './MovieList.styled';
+import ScrollToTopButton from '../ScrollToTopButton/ScrollToTopButton.js';
