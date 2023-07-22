d686adfdf1ddd30a2a969806cd78c5ca7ce0beac
Ihor Poliakov <piv2003@gmail.com>
2023-07-22 23:37:11 +0200

    Update Movies.jsx

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -6,3 +6,13 @@ import { Notification } from '../components/MovieList/MovieList.styled';
 import MovieList from '../components/MovieList/MovieList';
 import SearchBar from '../components/SearchBar/SearchBar';
 import Skeleton from '../components/Skeleton/Skeleton';
+
+const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [searched, setSearched] = useState(false);
+  const [noResults, setNoResults] = useState(false);
+  const movieName = searchParams.get('query') ?? '';
+  const [isLoading, setIsLoading] = useState(false);
+};
+export default Movies;
