b04cb03d34faa55150480c7621b29b7f635c9a50
Ihor Poliakov <piv2003@gmail.com>
2023-07-22 23:44:25 +0200

    adding MovieDetails

---
+++ src/pages/MovieDetails.jsx
@@ -0,0 +1,10 @@
+import { Suspense, useState, useEffect, useRef } from 'react';
+import { useParams, useLocation, Outlet } from 'react-router-dom';
+import { getMovieDetails } from '../components/App/App';
+import BackButton from '../components/BackButton/BackButton';
+import { StyledLink } from '../components/MovieDetails/MovieDetails.styled';
+import {
+  Container,
+  MovieInfo,
+  MovieDescription,
+} from '../components/MovieDetails/MovieDetails.styled';
