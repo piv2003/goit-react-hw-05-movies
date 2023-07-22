6adcfaf9fb61026d4bd3ec77a707267f45ad30dd
Ihor Poliakov <piv2003@gmail.com>
2023-07-22 23:34:06 +0200

    adding Movies

---
+++ src/pages/Movies.jsx
@@ -0,0 +1,4 @@
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { getSearchMovie } from '../components/App/App';
