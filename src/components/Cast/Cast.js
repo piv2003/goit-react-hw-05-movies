96eca6931fb098dae71f090b8777560c52ef1895
Ihor Poliakov <piv2003@gmail.com>
2023-07-22 13:53:22 +0200

    Update Cast.js

--- src/components/Cast/Cast.js
+++ src/components/Cast/Cast.js
@@ -7,6 +7,35 @@ import ScrollToTopButton from '../ScrollToTopButton/ScrollToTopButton.js';
 const Cast = () => {
   const { movieId } = useParams();
   const [actors, setActors] = useState([]);
+
+  useEffect(() => {
+    getMovieDetails(movieId, 'credits')
+      .then(credits => setActors(credits.cast))
+      .catch(console.log);
+  }, [movieId]);
+
+  return (
+    <>
+      <List>
+        <ScrollToTopButton />
+        {actors.map(actor => (
+          <li key={actor.cast_id}>
+            {!actor.profile_path ? (
+              <span>No photo</span>
+            ) : (
+              <img
+                src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`}
+                alt={actor.name}
+                width="100"
+              />
+            )}
+            <b>{actor.name}</b>
+            <p>{actor.character}</p>
+          </li>
+        ))}
+      </List>
+    </>
+  );
 };
 
 export default Cast;
