354798a9c574818bb995bb3e0d6afb771b45a187
Ihor Poliakov <piv2003@gmail.com>
2023-07-22 23:59:14 +0200

    Update MovieList.styled.jsx

---
+++ src/components/MovieList/MovieList.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  gap: 15px;
+  padding: 0 0 20px 0;
+  margin-top: 10px;
+`;
