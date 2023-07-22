10ceacf39d9d95c44ced320746b2dc42088908e4
Ihor Poliakov <piv2003@gmail.com>
2023-07-22 23:34:43 +0200

    Update Movies.jsx

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -2,3 +2,7 @@ import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { getSearchMovie } from '../components/App/App';
+import { Notification } from '../components/MovieList/MovieList.styled';
+import MovieList from '../components/MovieList/MovieList';
+import SearchBar from '../components/SearchBar/SearchBar';
+import Skeleton from '../components/Skeleton/Skeleton';
