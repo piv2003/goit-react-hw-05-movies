48ec62c94027dec766640e3e1ea718e1f9f37a0f
Ihor Poliakov <piv2003@gmail.com>
2023-07-22 13:35:44 +0200

    Update BackButton.js

--- src/components/BackButton/BackButton.js
+++ src/components/BackButton/BackButton.js
@@ -1,4 +1,14 @@
 import { HiOutlineChevronDoubleLeft } from 'react-icons/hi';
 import { StyledLink, Wrapper } from './BackButton.styled';
 
+const BackButton = ({ to, children }) => {
+  return (
+    <StyledLink to={to}>
+      <Wrapper>
+        <HiOutlineChevronDoubleLeft size="24" />
+      </Wrapper>
+    </StyledLink>
+  );
+};
+
 export default BackButton;
