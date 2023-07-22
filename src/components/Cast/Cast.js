e26dfb8a36a15bc770d445b85a5afcc98fc6ef48
Ihor Poliakov <piv2003@gmail.com>
2023-07-22 13:38:01 +0200

    adding Cast

---
+++ src/components/Cast/Cast.js
@@ -0,0 +1,5 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { getMovieDetails } from '../App/App';
+import { List } from './Cast.styled';
+import ScrollToTopButton from '../ScrollToTopButton/ScrollToTopButton.js';
