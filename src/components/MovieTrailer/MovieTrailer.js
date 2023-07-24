b4728dfa87ce240e9045d1bc0991ff3f5926925a
Ihor Poliakov <piv2003@gmail.com>
2023-07-24 15:32:43 +0200

    editing MovieTrailer

--- src/components/MovieTrailer/MovieTrailer.js
+++ src/components/MovieTrailer/MovieTrailer.js
@@ -7,6 +7,24 @@ import { MovieTrailerBox, Trailer } from './MovieTrailer.styled';
 const MovieTrailer = () => {
   const { movieId } = useParams();
   const [trailer, setTrailer] = useState(null);
+
+  useEffect(() => {
+    getMovieDetails(movieId, 'videos')
+      .then(data => {
+        const video = data.results.filter(item => item.type === 'Trailer')[0];
+
+        if (video) {
+          setTrailer(`https://www.youtube.com/embed/${video.key}`);
+        }
+      })
+      .catch(console.log);
+  }, [movieId, trailer]);
+
+  return (
+    <MovieTrailerBox>
+      <Trailer url={trailer} width="70%" height="70%" controls="true" />
+    </MovieTrailerBox>
+  );
 };
 
 export default MovieTrailer;
