b73299cd4b65696aff773ff4949c2b95f5907ef3
Ihor Poliakov <piv2003@gmail.com>
2023-07-24 13:23:02 +0200

    editing MovieList

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,35 @@
+import { Link, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Item, List } from './MovieList.styled';
+import ScrollToTopButton from '../ScrollToTopButton/ScrollToTopButton.js';
+
+const MovieList = ({ movies }) => {
+  const location = useLocation();
+
+  return (
+    <List>
+      <ScrollToTopButton />
+      {movies.map(({ id, title, poster_path }) => (
+        <Item key={id}>
+          <Link to={`/movies/${id}`} state={{ from: location }}>
+            <div>
+              {poster_path ? (
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+                  alt={`Poster ${title}`}
+                  width="250px"
+                  height="375px"
+                />
+              ) : (
+                <span>No poster</span>
+              )}
+              <b>{title}</b>
+            </div>
+          </Link>
+        </Item>
+      ))}
+    </List>
+  );
+};
+
+export default MovieList;
