3eac96590662038dc2d5c9b8a0ce3019c8a9fbcd
Ihor Poliakov <piv2003@gmail.com>
2023-07-25 23:18:17 +0200

    fixing errors

--- src/components/MovieTrailer/MovieTrailer.jsx
+++ src/components/MovieTrailer/MovieTrailer.jsx
@@ -7,22 +7,33 @@ import { MovieTrailerBox, Trailer } from './MovieTrailer.styled';
 const MovieTrailer = () => {
   const { movieId } = useParams();
   const [trailer, setTrailer] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     getMovieDetails(movieId, 'videos')
       .then(data => {
-        const video = data.results.filter(item => item.type === 'Trailer')[0];
+        const video = data.results.find(item => item.type === 'Trailer');
 
         if (video) {
           setTrailer(`https://www.youtube.com/embed/${video.key}`);
         }
+
+        setLoading(false); // Загрузка завершена
       })
       .catch(console.log);
   }, [movieId, trailer]);
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!trailer) {
+    return <div>No trailer available for this movie.</div>;
+  }
+
   return (
     <MovieTrailerBox>
-      <Trailer url={trailer} width="70%" height="70%" controls="true" />
+      <Trailer url={trailer} width="70%" height="70%" controls={true} />
     </MovieTrailerBox>
   );
 };
